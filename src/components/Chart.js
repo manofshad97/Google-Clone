38990064612f47884acadd7014bb08a21c573432
manofshad97 <shadman559@hotmail.com>
2022-02-10 21:28:23 -0500

    added hourly timestamps to 1D filter

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import ChartFilter from './ChartFilter'
+import * as usertz from 'user-timezone'
 import Card from './Card'
 import {
   Area,
@@ -32,7 +33,10 @@ const Chart = () => {
     return data.c.map((item, index) => {
       return {
         value: item.toFixed(2),
-        date: convertUnixTimestampToDate(data.t[index], includeYear),
+        date:
+          filter === '1D'
+            ? usertz.datetime(data.t[index], 'h:mm A')
+            : convertUnixTimestampToDate(data.t[index], includeYear),
       }
     })
   }
@@ -72,52 +76,52 @@ const Chart = () => {
 
   return (
     <Card>
-      <div  className="w-full h-full -mx-4">
-      <ul className="flex absolute top-2 right-2 z-40">
-        {Object.keys(chartConfig).map((item) => (
-          <li key={item}>
-            <ChartFilter
-              text={item}
-              active={filter === item}
-              onClick={() => {
-                setFilter(item)
-              }}
+      <div className="w-full h-full -mx-4">
+        <ul className="flex absolute top-2 right-2 z-40">
+          {Object.keys(chartConfig).map((item) => (
+            <li key={item}>
+              <ChartFilter
+                text={item}
+                active={filter === item}
+                onClick={() => {
+                  setFilter(item)
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+        <ResponsiveContainer>
+          <AreaChart data={data}>
+            <defs>
+              <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={darkMode ? '#312e81' : 'rgb(199 210 254)'}
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={darkMode ? '#312e81' : 'rgb(199 210 254)'}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <Tooltip
+              contentStyle={darkMode ? { backgroundColor: '#111827' } : null}
+              itemStyle={darkMode ? { color: '#818cf8' } : null}
             />
-          </li>
-        ))}
-      </ul>
-      <ResponsiveContainer>
-        <AreaChart data={data}>
-          <defs>
-            <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
-              <stop
-                offset="5%"
-                stopColor={darkMode ? '#312e81' : 'rgb(199 210 254)'}
-                stopOpacity={0.8}
-              />
-              <stop
-                offset="95%"
-                stopColor={darkMode ? '#312e81' : 'rgb(199 210 254)'}
-                stopOpacity={0}
-              />
-            </linearGradient>
-          </defs>
-          <Tooltip
-            contentStyle={darkMode ? { backgroundColor: '#111827' } : null}
-            itemStyle={darkMode ? { color: '#818cf8' } : null}
-          />
-          <Area
-            type="monotone"
-            dataKey="value"
-            stroke="#312e81"
-            fill="url(#chartColor)"
-            fillOpacity={1}
-            strokeWidth={0.5}
-          />
-          <XAxis dataKey="date" tickMargin={15} />
-          <YAxis domain={['dataMin', 'dataMax']} />
-        </AreaChart>
-      </ResponsiveContainer>
+            <Area
+              type="monotone"
+              dataKey="value"
+              stroke="#312e81"
+              fill="url(#chartColor)"
+              fillOpacity={1}
+              strokeWidth={0.5}
+            />
+            <XAxis dataKey="date" tickMargin={15} />
+            <YAxis domain={['dataMin', 'dataMax']} />
+          </AreaChart>
+        </ResponsiveContainer>
       </div>
     </Card>
   )
